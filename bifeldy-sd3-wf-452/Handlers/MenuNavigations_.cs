eacc10226bd25bc14b3877ee21658cd347b153ba
Bifeldy <bifeldy@gmail.com>
2022-12-15 14:10:40 +0700

    Haha Hihi

--- bifeldy-sd3-wf-452/Handlers/MenuNavigations_.cs
+++ bifeldy-sd3-wf-452/Handlers/MenuNavigations_.cs
@@ -70,7 +70,7 @@ namespace DcTransferFtpNew.Handlers {
                         // Change `navContent`
                         try {
                             if (!mainPanel.NavContent.Controls.ContainsKey(buttonMenuItem.Name)) {
-                                UserControl ctrl = (UserControl)CProgram.Bifeldyz.ResolveNamed(buttonMenuItem.Name);
+                                UserControl ctrl = (UserControl) CProgram.Bifeldyz.ResolveNamed(buttonMenuItem.Name);
                                 mainPanel.NavContent.Controls.Add(ctrl);
                             }
                             mainPanel.NavContent.Controls[buttonMenuItem.Name].BringToFront();
--- bifeldy-sd3-wf-452/Navigations/ProsesHarian.cs
+++ bifeldy-sd3-wf-452/Navigations/ProsesHarian.cs
@@ -53,7 +53,7 @@ namespace DcTransferFtpNew.Navigations {
             ButtonMenuHarianList.Add(new Button() { Name = "CProsesHarianDataIrpc", Text = "Data IRPC" });
             ButtonMenuHarianList.Add(new Button() { Name = "CProsesHarianFingerScan", Text = "Data FingerScan" });
             ButtonMenuHarianList.Add(new Button() { Name = "CProsesHarianTaxFull", Text = "Data Tax Full" });
-            ButtonMenuHarianList.Add(new Button() { Name = "CProsesHarianTaxRe", Text = "Data Tax Re" });
+            ButtonMenuHarianList.Add(new Button() { Name = "CProsesHarianTaxRe", Text = "Data Tax Re:" });
         }
 
     }
