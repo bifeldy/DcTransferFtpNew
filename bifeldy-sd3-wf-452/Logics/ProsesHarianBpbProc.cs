073b158ae6dd534273c7eef58e3c8fa02dbe1e65
Bifeldy <bifeldy@gmail.com>
2022-12-20 15:58:21 +0700

    Bpb Proc ~~

---
+++ bifeldy-sd3-wf-452/Logics/ProsesHarianBpbProc.cs
@@ -0,0 +1,87 @@
+﻿/**
+ * 
+ * Author       :: Basilius Bias Astho Christyono
+ * Mail         :: bias@indomaret.co.id
+ * Phone        :: (+62) 889 236 6466
+ * 
+ * Department   :: IT SD 03
+ * Mail         :: bias@indomaret.co.id
+ * 
+ * Catatan      :: Proses Harian Data BPB Proc
+ *              :: Harap Didaftarkan Ke DI Container
+ * 
+ */
+
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using bifeldy_sd3_lib_452.Models;
+using bifeldy_sd3_lib_452.Utilities;
+
+using DcTransferFtpNew.Abstractions;
+using DcTransferFtpNew.Handlers;
+
+namespace DcTransferFtpNew.Logics {
+
+    public interface IProsesHarianBpbProc : ILogics { }
+
+    public sealed class CProsesHarianBpbProc : CLogics, IProsesHarianBpbProc {
+
+        private readonly ILogger _logger;
+        private readonly IDb _db;
+        private readonly IBerkas _berkas;
+        private readonly IQTrfCsv _qTrfCsv;
+        private readonly IDcFtpT _dcFtpT;
+
+        public CProsesHarianBpbProc(
+            ILogger logger,
+            IDb db,
+            IBerkas berkas,
+            IQTrfCsv q_trf_csv,
+            IDcFtpT dc_ftp_t
+        ) : base(db) {
+            _logger = logger;
+            _db = db;
+            _berkas = berkas;
+            _qTrfCsv = q_trf_csv;
+            _dcFtpT = dc_ftp_t;
+        }
+
+        public override async Task Run(object sender, EventArgs e, Control currentControl) {
+            PrepareHarian(sender, e, currentControl);
+            await Task.Run(async () => {
+                if (IsDateRangeValid(dateStart, dateEnd)) {
+                    _berkas.DeleteOldFilesInFolder(_berkas.TempFolderPath, 0);
+                    JumlahServerKirimCsv = 1;
+
+                    int jumlahHari = (int)((dateEnd - dateStart).TotalDays + 1);
+                    _logger.WriteInfo(GetType().Name, $"{dateStart:MM/dd/yyyy} - {dateEnd:MM/dd/yyyy} ({jumlahHari} Hari)");
+
+                    for (int i = 0; i < jumlahHari; i++) {
+                        DateTime xDate = dateStart.AddDays(i);
+
+                        string procName = await _db.DC_FILE_SCHEDULER_T__GET("file_procedure", "BPBPROCUR");
+                        CDbExecProcResult res = await _db.CALL__P_TGL(procName, xDate);
+                        if (res == null || !res.STATUS) {
+                            throw new Exception($"Gagal Menjalankan Procedure {procName}");
+                        }
+
+                        if (await _qTrfCsv.CreateCSVFile("BPBPROCUR", seperator: "|")) {
+                            TargetKirim += JumlahServerKirimCsv;
+                        }
+                    }
+
+                    // string zipFileName = await _db.Q_TRF_CSV__GET($"{(_app.IsUsingPostgres ? "COALESCE" : "NVL")}(q_namazip, q_namafile)", "BPBPROCUR");
+                    // int totalFileInZip = _berkas.ZipListFileInTempFolder(zipFileName);
+
+                    BerhasilKirim += await _dcFtpT.KirimFtp("BPBPROCUR"); // *.CSV Sebanyak :: TargetKirim
+
+                    _berkas.CleanUp();
+                }
+            });
+            CheckHasilKiriman();
+        }
+
+    }
+}
