d6b73fe759aea8ee5e9d1c691c54dba0f9e5fa33
Bifeldy <bifeldy@gmail.com>
2022-12-30 08:17:47 +0700

    Unused Logger

--- bifeldy-sd3-wf-452/Abstractions/Navigations.cs
+++ bifeldy-sd3-wf-452/Abstractions/Navigations.cs
@@ -69,6 +69,7 @@ namespace DcTransferFtpNew.Abstractions {
                         }
                         catch (Exception ex) {
                             // _db.MarkFailedRollbackAndClose();
+                            // _logger.WriteError(ex);
                             MessageBox.Show(ex.Message, "Terjadi Kesalahan! (｡>﹏<｡)", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
 
