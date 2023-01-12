d0e35398d769e24857c0b35d30e258a86780f55d
Bifeldy <bifeldy@gmail.com>
2023-01-12 15:37:05 +0700

    Transfer Mstxhg ~

--- bifeldy-sd3-wf-452/Logics/ProsesBulananTransferMstxhg_.cs
+++ bifeldy-sd3-wf-452/Logics/ProsesBulananTransferMstxhg_.cs
@@ -7,7 +7,7 @@
  * Department   :: IT SD 03
  * Mail         :: bias@indomaret.co.id
  * 
- * Catatan      :: Proses Bulanan Data Bulanan
+ * Catatan      :: Proses Bulanan Mstxhg
  *              :: Harap Didaftarkan Ke DI Container
  * 
  */
@@ -59,8 +59,47 @@ namespace DcTransferFtpNew.Logics {
             _dcFtpT = dc_ftp_t;
         }
 
-        public override Task Run(object sender, EventArgs e, Control currentControl) {
-            throw new NotImplementedException("Fitur Belum Di-Implementasi-Kan ...");
+        public override async Task Run(object sender, EventArgs e, Control currentControl) {
+            PrepareBulanan(sender, e, currentControl);
+            await Task.Run(async () => {
+                _berkas.DeleteOldFilesInFolder(_berkas.TempFolderPath, 0);
+                JumlahServerKirimCsv = 1;
+                JumlahServerKirimZip = 1;
+
+                string fileTimeMSTXHGFormat = $"{dateStart:MM}";
+                string fileTimeMSTXHGGFormat = $"{dateStart:yyMM}";
+                string csvFileName = null;
+
+                string procName = "TRF_MSTXHG_EVO";
+                CDbExecProcResult res = await _db.CALL__P_TGL(procName, datePeriode);
+                if (res == null || !res.STATUS) {
+                    throw new Exception($"Gagal Menjalankan Procedure {procName}");
+                }
+
+                csvFileName = $"MSTXHG{fileTimeMSTXHGFormat}.CSV";
+                await _qTrfCsv.CreateCSVFile("MSTXHG", csvFileName);
+                TargetKirim += JumlahServerKirimCsv;
+
+                string zipFileName = await _db.Q_TRF_CSV__GET($"{(_app.IsUsingPostgres ? "COALESCE" : "NVL")}(q_namazip, q_namafile)", "MSTXHG") ?? "MSTXHG";
+                _berkas.ZipListFileInFolder(zipFileName);
+                TargetKirim += JumlahServerKirimZip;
+
+                BerhasilKirim += await _dcFtpT.KirimAllCsv("LOCAL"); // *.CSV Sebanyak :: TargetKirim
+                BerhasilKirim += await _dcFtpT.KirimFtpDev("HARIAN", zipFileName, true); // *.ZIP Sebanyak :: 1
+
+                csvFileName = $"MSTXHGG{fileTimeMSTXHGGFormat}.CSV";
+                await _qTrfCsv.CreateCSVFile("MSTXHGG", csvFileName);
+                // TargetKirim += JumlahServerKirimCsv;
+
+                zipFileName = await _db.Q_TRF_CSV__GET($"{(_app.IsUsingPostgres ? "COALESCE" : "NVL")}(q_namazip, q_namafile)", "MSTXHGG") ?? "MSTXHGG";
+                _berkas.ZipListFileInFolder(zipFileName);
+                TargetKirim += JumlahServerKirimZip;
+
+                BerhasilKirim += await _dcFtpT.KirimSingleZip("WRC", zipFileName); // *.ZIP Sebanyak :: 1
+
+                _berkas.CleanUp();
+            });
+            CheckHasilKiriman();
         }
 
     }
